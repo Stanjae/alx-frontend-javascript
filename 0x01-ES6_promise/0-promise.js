a78597d77a9f8fa71fb8c741f1694452a5306cc1
stanjae <stanykhay29@gmail.com>
2024-06-26 12:10:16 +0000

    socks

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,4 +1,4 @@
 export default async function getResponseFromAPI() {
-    const promise = new Promise(() => {});
-    return promise;
-  }
+	const promise = new Promise(() => {});
+	return promise;
+}
